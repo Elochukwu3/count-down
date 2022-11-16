6bf89d6b8f51f19dc644331f376ff4dc68209819
Elochukwu3 <oguajuromauld5@gmail.com>
2022-11-16 23:47:50 +0000

    style: saved style

--- src/timer-function/Timer.jsx
+++ src/timer-function/Timer.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from "react";
+import TimerUi from "./TimerUi";
 // import {updateTimer} from './UpdateTimer.js';
 const initialTime = {
   seconds: "00",
@@ -9,6 +10,7 @@ const initialTime = {
 export default function Timer() {
   const [time, setTime] = useState(initialTime);
   const [dateInput, setDateInput] = useState("2023-06-19");
+
   const countDownDate = new Date(dateInput).getTime();
   const todayDate = new Date().getTime();
   const dateInterval = countDownDate - todayDate;
@@ -16,6 +18,7 @@ export default function Timer() {
   const showInput = (e) => {
     setDateInput(e.target.value);
   };
+
   useEffect(() => {
     const interval = setInterval(() => {
       setTime(updateTimer(dateInterval));
@@ -25,17 +28,10 @@ export default function Timer() {
 
   return (
     <div>
-      <input type={"date"} onChange={showInput} value={dateInput} />
-
       {dateInterval < 0 ? (
-        "helloo"
+        <Timer/>
       ) : (
-        <ul>
-          <li>{time.days}</li>
-          <li>{time.hours}</li>
-          <li>{time.minutes}</li>
-          <li>{time.seconds}</li>
-        </ul>
+        <TimerUi/>
       )}
     </div>
   );
