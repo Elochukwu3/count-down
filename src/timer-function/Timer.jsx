11dfdeee6082d50270d36c80eb8ce516c7e70d65
Elochukwu3 <oguajuromauld5@gmail.com>
2022-11-16 22:54:34 +0000

    update: updated timer

--- src/timer-function/Timer.jsx
+++ src/timer-function/Timer.jsx
@@ -1,72 +1,75 @@
-import {useState, useEffect, useRef} from 'react';
+import { useState, useEffect, useRef } from "react";
 // import {updateTimer} from './UpdateTimer.js';
 const initialTime = {
-    seconds: '00',
-    minutes: '00',
-    hours: '00',
-    days: '00'
-}
+  seconds: "00",
+  minutes: "00",
+  hours: "00",
+  days: "00",
+};
 export default function Timer() {
-    const [time, setTime] = useState(initialTime);
-    const [dateInput, setDateInput] = useState("2023-06-19");
-    const countDownDate = new Date(dateInput).getTime();
-    const todayDate = new Date().getTime()
-   const dateInterval = countDownDate - todayDate;
+  const [time, setTime] = useState(initialTime);
+  const [dateInput, setDateInput] = useState("2023-06-19");
+  const countDownDate = new Date(dateInput).getTime();
+  const todayDate = new Date().getTime();
+  const dateInterval = countDownDate - todayDate;
 
-  const showInput=(e)=>{ 
-   
-    setDateInput(e.target.value)
-   }
-   useEffect(()=>{
-const interval = setInterval(() => {setTime(updateTimer( dateInterval ))}, 1000);
-return() => clearInterval(interval)
- }, [ dateInterval ])
+  const showInput = (e) => {
+    setDateInput(e.target.value);
+  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(updateTimer(dateInterval));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [dateInterval]);
 
   return (
     <div>
-      <input type={"date"}  onChange={showInput} value={dateInput}/>
-      
-      {
-        dateInterval < 0 ? "helloo": (
+      <input type={"date"} onChange={showInput} value={dateInput} />
+
+      {dateInterval < 0 ? (
+        "helloo"
+      ) : (
         <ul>
           <li>{time.days}</li>
           <li>{time.hours}</li>
           <li>{time.minutes}</li>
           <li>{time.seconds}</li>
-        </ul>)
-      }
+        </ul>
+      )}
     </div>
-  )
+  );
 }
 
 function updateTimer(countDown) {
-  return{
+  return {
     seconds: convertSecond(countDown),
     minutes: convertMin(countDown),
-    hours:convertHr(countDown),
-    days: convertDay(countDown)
-  }
+    hours: convertHr(countDown),
+    days: convertDay(countDown),
+  };
 }
 
 function convertSecond(countDown) {
   const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
- return  addWithZeros(seconds, 2)
-  
+  return addWithZeros(seconds, 2);
 }
 function convertMin(countDown) {
   const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
-  return addWithZeros(minutes, 2)
+  return addWithZeros(minutes, 2);
 }
 function convertHr(countDown) {
-  const hours = Math.floor((countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  return addWithZeros(hours, 2)
+  const hours = Math.floor(
+    (countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  return addWithZeros(hours, 2);
 }
 function convertDay(countDown) {
-  const days = Math.floor((countDown /(1000 * 60 *60 * 24)));
-  return days.toString()
+  const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+  return days.toString();
 }
 function addWithZeros(number, minLength) {
   const numberString = number.toString();
-  if(numberString.length >= minLength) return numberString;
-  return "0".repeat(minLength - numberString.length) +  numberString;
+  if (numberString.length >= minLength) return numberString;
+  return "0".repeat(minLength - numberString.length) + numberString;
 }
