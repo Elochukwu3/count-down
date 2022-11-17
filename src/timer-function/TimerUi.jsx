db5dfa298add11b2fe5696e2fa3184c75b998fa7
Elochukwu3 <oguajuromauld5@gmail.com>
2022-11-17 13:26:03 +0100

    style: style

--- src/timer-function/TimerUi.jsx
+++ src/timer-function/TimerUi.jsx
@@ -5,12 +5,25 @@ export default function TimerUi({showInput, dateInput}) {
   return (
     <div className='page-container'>
         <div className='page-wrapper'>
-          <img src="./img/countd.jpg" alt="" />
+          {/* <img src="./img/countd.jpg" alt="" /> */}
         <input type={"date"} onChange={showInput} value={dateInput} />
         <ul>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
+            <li>
+              <p>00</p>
+              <p>days</p>
+            </li>
+            <li>
+              <p>00</p>
+              <p>Hrs</p>
+            </li>
+            <li>
+            <p>00</p>
+              <p>Mins</p>
+            </li>
+            <li>
+               <p>00</p>
+              <p>Secs</p>
+            </li>
         </ul>
        
         </div>
