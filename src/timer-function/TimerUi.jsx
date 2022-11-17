f62281b16ef06d70a96420fe8182aa96733e2796
Elochukwu3 <oguajuromauld5@gmail.com>
2022-11-17 21:09:46 +0100

    style: all styles

--- src/timer-function/TimerUi.jsx
+++ src/timer-function/TimerUi.jsx
@@ -1,33 +1,33 @@
-import React from 'react'
-import './Style.css';
+import React from "react";
+import "./Style.css";
 
-export default function TimerUi({dateVal, dateChange, hour, min, sec, day}) {
-
+export default function TimerUi({ dateVal, dateChange, hour, min, sec, day }) {
   return (
-    <div className='page-container'>
-        <div className='page-wrapper'>
-         
+    <div className="page-container">
+      <div>
+        <p className="instruction">instruction: <span> (i) select a date to count down to</span></p>
+      </div>
+      <div className="page-wrapper">
         <input type={"date"} onChange={dateChange} value={dateVal} />
         <ul>
-            <li>
-              <p>{day}</p>
-              <p>days</p>
-            </li>
-            <li>
-              <p>{hour}</p>
-              <p>Hrs</p>
-            </li>
-            <li>
-            <p>{min}</p>
-              <p>Mins</p>
-            </li>
-            <li>
-               <p>{sec}</p>
-              <p>Secs</p>
-            </li>
+          <li>
+            <p className="num">{day}</p>
+            <p className="sub">days</p>
+          </li>
+          <li>
+            <p className="num">{hour}</p>
+            <p className="sub">Hrs</p>
+          </li>
+          <li>
+            <p className="num">{min}</p>
+            <p className="sub"> Mins</p>
+          </li>
+          <li>
+            <p className="num">{sec}</p>
+            <p className="sub">Secs</p>
+          </li>
         </ul>
-       
-        </div>
+      </div>
     </div>
-  )
+  );
 }
