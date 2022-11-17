5c50e8b10bd406e13c44e505522982db379cb235
Elochukwu3 <oguajuromauld5@gmail.com>
2022-11-17 00:33:59 +0000

    feature

--- src/timer-function/TimerUi.jsx
+++ src/timer-function/TimerUi.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import '.Style.css';
-import {backImg} from '../img/countd.jpeg'
+import './Style.css';
+
 export default function TimerUi({showInput, dateInput}) {
   return (
-    <div>
+    <div className='page-container'>
+        <div className='page-wrapper'>
+          <img src="./img/countd.jpg" alt="" />
         <input type={"date"} onChange={showInput} value={dateInput} />
         <ul>
-            <li></li>
+            <li>Hello</li>
+            <li>Hello</li>
+            <li>Hello</li>
         </ul>
+       
+        </div>
     </div>
   )
 }
